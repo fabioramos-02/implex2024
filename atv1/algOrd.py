c9d2a88b48e07104a4fc0b5a2a17d655f97ecff8
Fabio Ramos <fabiowilliam466@gmail.com>
2024-09-09 10:58:49 -0400

    TESTE

--- atv1/algOrd.py
+++ atv1/algOrd.py
@@ -1,17 +1,28 @@
 import random
 # ALGORITMO DE ORDENAÇÃO COUNTINGSORT
 def counting_sort(arr):
+    if len(arr) == 0:  # Verificar se o array não está vazio
+        return arr
+    
+    # Encontrar o valor máximo no array
     max_val = max(arr)
+    
+    # Inicializar o array de contagem
     count = [0] * (max_val + 1)
-
-    while len(arr) > 0:
-        num = arr.pop(0)
+    
+    # Contar as ocorrências de cada valor
+    for num in arr:
         count[num] += 1
-
+    
+    # Reconstruir o array original ordenado
+    index = 0
     for i in range(len(count)):
         while count[i] > 0:
-            arr.append(i)
+            arr[index] = i
+            index += 1
             count[i] -= 1
+
+    return arr
 
 
 # ALGORITMO DE ORDENAÇÃO HEAPSORT
